75e4c658d04184f10e3df4145374a05728077124
Alex27ak <96329236+Alex27ak@users.noreply.github.com>
2022-09-19 22:02:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -717,7 +717,7 @@ async def auto_filter(client, msg, spoll=False):
         BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
-            [InlineKeyboardButton(text=f"🌹 1/{math.ceil(int(total_results) / 10)} 🌟", callback_data="pages"),
+            [InlineKeyboardButton(text=f"🌹 1/{math.ceil(int(total_results) / 10)} ", callback_data="pages"),
              InlineKeyboardButton(text="NEXT ⏩", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
