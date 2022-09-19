8a629fa079706ec8d037271ea9cc022156ac894f
Alex27ak <96329236+Alex27ak@users.noreply.github.com>
2022-09-19 21:54:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -73,10 +73,10 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file.file_name}✮࿐", callback_data=f'files#{file.file_id}'
+                    text=f"—͟͞͞★{file.file_name}✮࿐", callback_data=f'files#{file.file_id}'
                 ),
                 InlineKeyboardButton(
-                    text=f"—͟͞͞★{get_size(file.file_size)}",
+                    text=f"—͟͞͞★{get_size(file.file_size)}✮࿐",
                     callback_data=f'files_#{file.file_id}',
                 ),
             ]
@@ -701,11 +701,11 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file.file_name}✮࿐",
+                    text=f"—͟͞͞★{file.file_name}✮࿐",
                     callback_data=f'{pre}#{file.file_id}',
                 ),
                 InlineKeyboardButton(
-                    text=f"—͟͞͞★{get_size(file.file_size)}",
+                    text=f"—͟͞͞★{get_size(file.file_size)}✮࿐",
                     callback_data=f'{pre}#{file.file_id}',
                 ),
             ]
@@ -717,12 +717,12 @@ async def auto_filter(client, msg, spoll=False):
         BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
-            [InlineKeyboardButton(text=f"🌟 1/{math.ceil(int(total_results) / 10)} 🌟", callback_data="pages"),
+            [InlineKeyboardButton(text=f"🌹 1/{math.ceil(int(total_results) / 10)} 🌟", callback_data="pages"),
              InlineKeyboardButton(text="NEXT ⏩", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
         btn.append(
-            [InlineKeyboardButton(text="🌟 1/1 🌟", callback_data="pages")]
+            [InlineKeyboardButton(text="🌹1/1", callback_data="pages")]
         )
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
