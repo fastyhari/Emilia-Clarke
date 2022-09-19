a627be68e3c6ad58998fb6d21424bbbd57305c93
Alex27ak <96329236+Alex27ak@users.noreply.github.com>
2022-09-19 16:52:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -250,6 +250,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚡JOIN NOW⚡', url='https://t.me/aklinksz') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
